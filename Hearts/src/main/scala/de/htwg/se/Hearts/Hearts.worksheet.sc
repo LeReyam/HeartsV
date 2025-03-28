f6a06d7c92531b3ebd05f1570af37cbbaed26f1b
LeReyam <Leon.Mayer@htwg-konstanz.de>
2025-03-28 11:47:51 +0100

    Git IgnoreFile

---
+++ Hearts/src/main/scala/de/htwg/se/Hearts/Hearts.worksheet.sc
@@ -0,0 +1,5 @@
+println("Hello, world!")
+val y = 2.3
+val x = 1
+x + x
+x + y
---
+++ Hearts/src/main/scala/de/htwg/se/Hearts/Main.scala
@@ -0,0 +1,15 @@
+package de.htwg.se.Hearts
+@main def hello(): Unit =
+  println()
+  println("Round 1:")
+  print("Stich: ")
+  println(stich)
+  print("Hand: ")
+  println(Hand)
+  println("Card to play: ")
+
+//def msg = "I was compiled by Scala 3. :)"
+
+def stich = "2Kreuz,5Kreuz,DamePik,"
+
+def Hand = "4Kreuz,7Kreuz,5Pik,BubePik,5Herz,7Herz"
