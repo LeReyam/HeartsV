31fa342210e5e66f765b158213225d85af8999eb
LeReyam <Leon.Mayer@htwg-konstanz.de>
2025-04-11 10:16:36 +0200

    Implemted tests in PlayerSpec for player

--- Hearts/src/test/scala/de/htwg/se/Hearts/model/PlayerSpec.scala
+++ Hearts/src/test/scala/de/htwg/se/Hearts/model/PlayerSpec.scala
@@ -1,2 +1,27 @@
 package de.htwg.se.Hearts.model
 
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.must.Matchers
+
+class PlayerSpec extends AnyWordSpec with Matchers {
+
+  "A Player" should {
+
+    "return name as toString" in {
+      val player = Player("Alice")
+      player.toString mustBe "Alice"
+    }
+
+    "have an empty hand by default" in {
+      val player = Player("Bob")
+      player.hand mustBe empty
+    }
+
+    "allow modifying the hand" in {
+      val player = Player("Charlie")
+      val card = Card(rank = "10", suit = Suit.Hearts)
+      player.hand = List(card)
+      player.hand mustBe List(card)
+    }
+  }
+}
