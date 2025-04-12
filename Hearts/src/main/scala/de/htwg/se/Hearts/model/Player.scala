119a7dd78a22bdab4dfbfe710a2fb10b166a470f
LeReyam <Leon.Mayer@htwg-konstanz.de>
2025-04-12 19:31:09 +0200

    reformating

--- Hearts/src/main/scala/de/htwg/se/Hearts/model/Player.scala
+++ Hearts/src/main/scala/de/htwg/se/Hearts/model/Player.scala
@@ -3,6 +3,18 @@ import de.htwg.se.Hearts.model.Card
 import scala.collection.mutable.ListBuffer
 
 case class Player(name: String, hand: List[Card]){
+    var points: Int = 0
+
+
+
+
+
+
+
+
+
+
+
     override def toString(): String = {
     val handStr = hand.map(card => s"  - $card").mkString("\n")
     s"$name's hand:\n$handStr"
