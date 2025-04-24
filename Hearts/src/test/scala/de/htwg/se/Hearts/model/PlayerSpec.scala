6fc81b0d1fcd4870f2dace1525b3986e48b838a7
LeReyam <Leon.Mayer@htwg-konstanz.de>
2025-04-24 15:06:53 +0200

    Added test for player play hand in PlayerSpec

--- Hearts/src/test/scala/de/htwg/se/Hearts/model/PlayerSpec.scala
+++ Hearts/src/test/scala/de/htwg/se/Hearts/model/PlayerSpec.scala
@@ -13,5 +13,10 @@ class PlayerSpec extends AnyWordSpec with Matchers {
       val player = new Player("Alice", List(Card(suit = Suit.Hearts,rank = Rank.Ten)))
       player.showHandString() should be ("Alice's hand: 10\u2665\n")
     }
+    "update hand after playing a Card" in {
+      val player = new Player("Alice", List(Card(suit = Suit.Hearts,rank = Rank.Ten),Card(suit = Suit.Hearts,rank = Rank.Five)))
+      player.playCard(Card(Rank.Five,Suit.Hearts))
+      player.hand should contain theSameElementsAs (List(Card(suit = Suit.Hearts,rank = Rank.Ten)))
+    }
   }
 }
