d1b539a51962319f5fb5f25b493f05d9a027cc7c
LeReyam <Leon.Mayer@htwg-konstanz.de>
2025-05-02 09:58:14 +0200

    Update output formating

--- Hearts/src/main/scala/de/htwg/se/Hearts/Main.scala
+++ Hearts/src/main/scala/de/htwg/se/Hearts/Main.scala
@@ -5,11 +5,14 @@ import de.htwg.se.Hearts.game._
 @main def runHearts(): Unit =
   val alice = new Player("Alice", List(
     Card(Rank.Two, Suit.Hearts),
-    Card(Rank.Ace, Suit.Spades)
+    Card(Rank.Ace, Suit.Spades),
+    Card(Rank.Jack, Suit.Diamonds)
+
   ))
   val bob = new Player("Bob", List(
     Card(Rank.Ten, Suit.Clubs),
-    Card(Rank.King, Suit.Diamonds)
+    Card(Rank.King, Suit.Diamonds),
+    Card(Rank.Queen, Suit.Hearts)
   ))
 
   val game = new Game(List(alice, bob))
--- Hearts/src/main/scala/de/htwg/se/Hearts/view/ConsoleView.scala
+++ Hearts/src/main/scala/de/htwg/se/Hearts/view/ConsoleView.scala
@@ -6,23 +6,44 @@ import scala.collection.mutable.ListBuffer
 class ConsoleView extends View {
 
   override def displayPlayerHand(player: Player): Unit = {
-    val handStr = player.hand.map(card => s"${card.rank.toString}${card.suit.toString}").mkString(" | ")
+    // Formatiere jede Karte mit fester Breite (3 Zeichen + Padding)
+    val handStr = player.hand.map { card =>
+      val cardStr = s"${card.rank.toString}${card.suit.toString}"
+      // Füge Leerzeichen hinzu, um eine einheitliche Breite zu gewährleisten
+      f"$cardStr%-3s"
+    }.mkString(" | ")
     println(s"${player.name}\t| $handStr |")
   }
 
   // Diese Methode erwartet jetzt `Players` und den aktuellen Pott
   override def displayGameState(players: Players, currentPot: ListBuffer[Card]): Unit = {
-    println("-" * 50)
-    println(f"\t\t| 1\t\t| 2\t\t|")  // Hier kannst du mehr Spalten einfügen, wenn du mehr Spieler hast
+    println("-" * 100)
+
+    // Dynamische Generierung der Spaltenüberschriften basierend auf der Länge der Hand des letzten Spielers
+    val headerBuilder = new StringBuilder("\t")
+    // Prüfen, ob es Spieler gibt und ob der letzte Spieler eine Hand hat
+    if (players.players.nonEmpty) {
+      val lastPlayer = players.players.last
+      for (i <- 0 to lastPlayer.hand.length - 1) {
+        headerBuilder.append(f"|  $i  ")
+      }
+      headerBuilder.append("|")
+    }
+    println(headerBuilder.toString())
 
     // Spielerhände anzeigen
     players.players.foreach(displayPlayerHand)
+    println("-" * 100)
+    // Anzeige des aktuellen Potts mit einheitlicher Formatierung
 
-    // Anzeige des aktuellen Potts
-    val potStr = currentPot.map(card => s"${card.rank.toString}${card.suit.toString}").mkString(" | ")
+    val potStr = currentPot.map { card =>
+      val cardStr = s"${card.rank.toString}${card.suit.toString}"
+      // Füge Leerzeichen hinzu, um eine einheitliche Breite zu gewährleisten
+      f"$cardStr%-3s"
+    }.mkString(" | ")
     println(f"Current Pott :\t| $potStr\t|")
 
-    println("-" * 50)
+    println("-" * 100)
   }
 
   override def displayMessage(message: String): Unit = {
