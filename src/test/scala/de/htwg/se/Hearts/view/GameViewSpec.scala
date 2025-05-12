9d8fb955223134909f7ba715e3c7a9daa60864ff
LeReyam <Leon.Mayer@htwg-konstanz.de>
2025-05-12 13:21:45 +0200

    Tests for the state of the trick added to GameViewSpec

--- src/test/scala/de/htwg/se/Hearts/view/GameViewSpec.scala
+++ src/test/scala/de/htwg/se/Hearts/view/GameViewSpec.scala
@@ -27,6 +27,58 @@ class GameViewSpec extends AnyWordSpec with Matchers {
         val outputString = gameView.createGameFrame()
 
         outputString should include regex """(?s)Alice.*?2\s*\u2665.*?A\s*\u2660.*?J\s*\u2666.*?Bob.*?10\s*\u2663.*?K\s*\u2666.*?Q\s*\u2665"""
+
+      }
+
+
+
+      "Display the current Pot if there are cards in it" in {
+        val alice = new Player("Alice", List(
+          Card(Rank.Two, Suit.Hearts),
+          Card(Rank.Ace, Suit.Spades),
+          Card(Rank.Jack, Suit.Diamonds)
+        ))
+        val bob = new Player("Bob", List(
+          Card(Rank.Ten, Suit.Clubs),
+          Card(Rank.King, Suit.Diamonds),
+          Card(Rank.Queen, Suit.Hearts)
+        ))
+        val game = new Game(List(alice, bob))
+        val controller = new GameController(game)
+        val gameView = new GameView(controller)
+
+        // Alice spielt die erste Karte (2♥)
+        controller.playCard(0)
+        // Bob spielt die erste Karte (10♣)
+        controller.playCard(0)
+
+        val outputString = gameView.createGameFrame()
+
+        // Überprüfen, ob der Pot die gespielten Karten enthält
+        outputString should include regex """(?s)Aktueller Pot:.*?2\s*\u2665.*?10\s*\u2663"""
+      }
+
+
+
+
+      "Display the empty pot correctly" in {
+        val alice = new Player("Alice", List(
+          Card(Rank.Two, Suit.Hearts),
+          Card(Rank.Ace, Suit.Spades),
+          Card(Rank.Jack, Suit.Diamonds)
+        ))
+        val bob = new Player("Bob", List(
+          Card(Rank.Ten, Suit.Clubs),
+          Card(Rank.King, Suit.Diamonds),
+          Card(Rank.Queen, Suit.Hearts)
+        ))
+        val game = new Game(List(alice, bob))
+        val controller = new GameController(game)
+        val gameView = new GameView(controller)
+
+        val outputString = gameView.createGameFrame()
+
+        outputString should include regex "Leer".r
       }
     }
 }
