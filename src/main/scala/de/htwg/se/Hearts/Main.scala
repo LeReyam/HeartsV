362740645a46197ff6183897731c7413c9fbeab3
xxFlo04 <florian.dunstheimer@gmail.com>
2025-06-12 20:07:22 +0200

    Gameview and Tui parallel

--- src/main/scala/de/htwg/se/Hearts/Main.scala
+++ src/main/scala/de/htwg/se/Hearts/Main.scala
@@ -1,32 +1,32 @@
-/*import de.htwg.se.Hearts.model._
-import de.htwg.se.Hearts.view._
-import de.htwg.se.Hearts.controller._
-
-@main def runHearts(): Unit =
-  val controller = new GameController()
-  val gameView = new GameView(controller)
-  controller.runGame()*/
 package de.htwg.se.Hearts
 
-import scalafx.application.JFXApp3
 import de.htwg.se.Hearts.controller.GameController
-import de.htwg.se.Hearts.view.Gui
+import de.htwg.se.Hearts.view.{Gui, GameView}
+import scalafx.application.JFXApp3
+import scalafx.scene.Scene
 
 object Main extends JFXApp3 {
 
-  val controller = new GameController()
-
-  // TUI in separatem Thread
-  new Thread(() => {
-    scala.io.Source.stdin.getLines().foreach(controller.handleInput)
-  }).start()
-
-  // GUI starten
   override def start(): Unit = {
+    val controller = new GameController()
     val gui = new Gui(controller)
+    val tui = new GameView(controller)
+
     stage = new JFXApp3.PrimaryStage {
       title = "Hearts"
       scene = gui.scene
     }
+
+    new Thread(() => {
+      var running = true
+      while (running) {
+        val input = scala.io.StdIn.readLine()
+        controller.handleInput(input)
+        if (controller.getCurrentState().startsWith("GameOverState")) {
+          println("TUI beendet – Spiel ist vorbei.")
+          running = false
+        }
+      }
+    }).start()
   }
 }
