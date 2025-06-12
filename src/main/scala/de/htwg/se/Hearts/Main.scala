21ee7c2a0258f3128d29ea7bc4ff70379d63affb
xxFlo04 <florian.dunstheimer@gmail.com>
2025-06-12 19:29:24 +0200

    GameView and Gui in one main but only one runs

--- src/main/scala/de/htwg/se/Hearts/Main.scala
+++ src/main/scala/de/htwg/se/Hearts/Main.scala
@@ -1,8 +1,32 @@
-import de.htwg.se.Hearts.model._
+/*import de.htwg.se.Hearts.model._
 import de.htwg.se.Hearts.view._
 import de.htwg.se.Hearts.controller._
 
 @main def runHearts(): Unit =
   val controller = new GameController()
   val gameView = new GameView(controller)
-  controller.runGame()
+  controller.runGame()*/
+package de.htwg.se.Hearts
+
+import scalafx.application.JFXApp3
+import de.htwg.se.Hearts.controller.GameController
+import de.htwg.se.Hearts.view.Gui
+
+object Main extends JFXApp3 {
+
+  val controller = new GameController()
+
+  // TUI in separatem Thread
+  new Thread(() => {
+    scala.io.Source.stdin.getLines().foreach(controller.handleInput)
+  }).start()
+
+  // GUI starten
+  override def start(): Unit = {
+    val gui = new Gui(controller)
+    stage = new JFXApp3.PrimaryStage {
+      title = "Hearts"
+      scene = gui.scene
+    }
+  }
+}
