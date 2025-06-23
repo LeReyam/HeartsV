11a57c7c3fa039e434ba12cef4741331740b5391
xxFlo04 <florian.dunstheimer@gmail.com>
2025-06-23 15:05:25 +0200

    changed pngs to smaller size, changed code in small py script

--- src/main/resources/convert_svg_to_png.py
+++ src/main/resources/convert_svg_to_png.py
@@ -1,12 +1,13 @@
-import os
-import cairosvg
+import os, re
+import cairosvg #needed for this to run. if you don't have it: pip install cairosvg
+
 
 # Edit the folder paths below
 svg_folder = r"yourpath/HeartsV/src/main/resources/cardssrc"
 png_folder = r"yourpath/HeartsV/src/main/resources/cards"
 
 # Size of PNGs
-width, height = 150, 210
+width, height = 90, 150
 
 # Create the destination folder if it doesn't exist
 os.makedirs(png_folder, exist_ok=True)
@@ -15,7 +16,13 @@ os.makedirs(png_folder, exist_ok=True)
 for filename in os.listdir(svg_folder):
     if filename.lower().endswith(".svg"):
         svg_path = os.path.join(svg_folder, filename)
-        png_filename = filename.replace(".svg", ".png")
+
+        base_name = filename[:-4]
+
+        if re.match(r"(jack|queen|king)_of_\w+2", base_name):
+            base_name = base_name[:-1]
+
+        png_filename = base_name + ".png"
         png_path = os.path.join(png_folder, png_filename)
 
         print(f"Converting: {filename} → {png_filename}")
