87725f35a5aa0291c595359011b204dd890a869f
xxFlo04 <florian.dunstheimer@gmail.com>
2025-06-23 14:16:50 +0200

    Corrected funtion fileName so that we get the correct string

--- src/main/scala/de/htwg/se/Hearts/model/Suit.scala
+++ src/main/scala/de/htwg/se/Hearts/model/Suit.scala
@@ -3,7 +3,11 @@ package de.htwg.se.Hearts.model
 enum Suit extends Ordered[Suit]:
   case Hearts, Spades, Diamonds, Clubs
 
-  def fileName: String = this.toString.toLowerCase 
+  def fileName: String = this match
+  case Hearts   => "hearts"
+  case Spades   => "spades"
+  case Diamonds => "diamonds"
+  case Clubs    => "clubs"
 
   override def compare(that: Suit): Int =
     this.ordinal.compare(that.ordinal)
